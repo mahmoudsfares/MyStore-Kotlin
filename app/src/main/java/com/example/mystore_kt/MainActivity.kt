0582c9a7b56a27e770198e53c9cf2b2f18c6b6bb
MSaeed <mahmoudsaeed1996@gmail.com>
2022-08-23 14:33:53 +0200

    Implement splash screen

--- app/src/main/java/com/example/mystore_kt/MainActivity.kt
+++ app/src/main/java/com/example/mystore_kt/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mystore_kt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.mystore_kt.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,11 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         installSplashScreen().apply {
             this.setKeepOnScreenCondition {
                 viewModel.isLoading.value
             }
         }
+        setContentView(R.layout.activity_main)
     }
 }
