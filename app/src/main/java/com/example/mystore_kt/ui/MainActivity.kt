afebcf500617da811abb0063ef7be82301278130
MSaeed <mahmoudsaeed1996@gmail.com>
2022-09-08 16:42:23 +0200

    connect navigation to toolbar

--- app/src/main/java/com/example/mystore_kt/ui/MainActivity.kt
+++ app/src/main/java/com/example/mystore_kt/ui/MainActivity.kt
@@ -3,7 +3,13 @@ package com.example.mystore_kt.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.widget.Toolbar
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.mystore_kt.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,5 +26,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
         setContentView(R.layout.activity_main)
+
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+
     }
 }
